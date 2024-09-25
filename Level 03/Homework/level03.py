ae71dae2c0e1b61284bdf2d112a7c12100d14777
nikamonaselidze <EpicGamesMonaselidze@gmail.com>
2024-09-25 21:50:26 +0400

    third commit

---
+++ Level 03/Homework/level03.py
@@ -0,0 +1,97 @@
+#ვქმნით 5 ცვლადს რომლებშიც შევინახავთ ჩვენი ოჯახის წევრების სახელებს (მე 9 გავაკეთე)
+
+Mother = "Lali"
+
+print("My Mother Is" + " " + Mother)
+
+Father = "Irakli"
+
+print("My Father Is" + " " + Father)
+
+Sister = "Anastasia"
+
+print("My Sister Is" + " " + Sister)
+
+Grandma1 = "Tinatini"
+
+print("My Grandma Is" + " " + Grandma1)
+
+Grandma2 = "Nana"
+
+print("My Grandma Is" + " " + Grandma2)
+
+Grandpa1 = "Konstantine"
+
+print("My Grandpa Is" + " " + Grandpa1)
+
+Grandpa2 = "Kukuri"
+
+print("My Grandpa Is" + " " + Grandpa2)
+
+Uncle = "Levani"
+
+print("My Uncle Is" + " " + Uncle)
+
+Cousin1 = "Giorgi"
+
+print("My Cousin Is" + " " + Cousin1)
+
+Cousin2 = "Luka"
+
+print("My Cousin Is" + " " + Cousin2)
+
+
+
+#ვასრულებთ ჩვენთვის სასურველ რიცხვებს შორის ოპერაციებს, მინიმუმ 5 თითოეულ ოპერატორზე (+,-,*,//)
+
+
+#მიმატება (+)
+
+print(7 + 1)
+
+print(19 + 31)
+
+print(42 + 11)
+
+print(5 + 17)
+
+print(99 + 1)
+
+
+#გამოკლება (-)
+
+print(11 - 2)
+
+print(100 - 50)
+
+print(71 - 27)
+
+print(161 - 92)
+
+print(999 - 666)
+
+
+#გამრავლება (*)
+
+print(9 * 9)
+
+print(11 * 11)
+
+print(3 * 9)
+
+print(7 * 6)
+
+print(8 * 32)
+
+
+#გაყოფა (//)
+
+print(9 // 3)
+
+print(33 // 11)
+
+print(99 // 33)
+
+print(1000 // 100)
+
+print(49 // 7)
